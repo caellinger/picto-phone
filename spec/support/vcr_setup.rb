1bbff1328fe1bf19f6d7dc3a360195e659eb3167
caellinger <c.a.ellinger@gmail.com>
2020-05-11 21:03:06 -0400

    updated tests, including adding VCR for calling external api endpoint

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -3,4 +3,10 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'vcr_cassettes'
   c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.default_cassette_options = { record: :new_episodes }
+  c.filter_sensitive_data('<AWS_ACCESS_KEY_ID>') { ENV['AWS_ACCESS_KEY_ID'] }
+  c.filter_sensitive_data('<AWS_SECRET_ACCESS_KEY>') { ENV['AWS_SECRET_ACCESS_KEY'] }
+  c.filter_sensitive_data('<PRODUCTION_S3_BUCKET>') { ENV['PRODUCTION_S3_BUCKET'] }
+  c.filter_sensitive_data('<DEVELOPMENT_S3_BUCKET>') { ENV['DEVELOPMENT_S3_BUCKET'] }
 end
