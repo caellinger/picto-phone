df0d0fe072bceffee1ba6294712b0ffd407fccb4
caellinger <c.a.ellinger@gmail.com>
2020-05-11 21:35:53 -0400

    fixed issue with cassette

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -9,4 +9,5 @@ VCR.configure do |c|
   c.filter_sensitive_data('<AWS_SECRET_ACCESS_KEY>') { ENV['AWS_SECRET_ACCESS_KEY'] }
   c.filter_sensitive_data('<PRODUCTION_S3_BUCKET>') { ENV['PRODUCTION_S3_BUCKET'] }
   c.filter_sensitive_data('<DEVELOPMENT_S3_BUCKET>') { ENV['DEVELOPMENT_S3_BUCKET'] }
+  c.filter_sensitive_data('<WORDNIK_API_KEY>') { ENV['WORDNIK_API_KEY'] }
 end
