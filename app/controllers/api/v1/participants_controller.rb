3850899cc4f15c5fc30d5d9d2a5b105a33dd0d5c
caellinger <c.a.ellinger@gmail.com>
2020-05-14 20:35:01 -0400

    updated logic to only allow 5 users per round and made some styling updates to help guide users

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -3,14 +3,29 @@ class Api::V1::ParticipantsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def create
-    if Participant.where(round_id: create_participant_params[:round_id]).where(user_id: create_participant_params[:user_id])[0]
-      render json: { round: Round.find(create_participant_params[:round_id]) }
+    if Round.find(create_participant_params[:round_id]).participants.count > 4
+      render json: {
+        rounds: Round.find_by_sql("
+          select
+            rounds.id,
+            rounds.starter_name
+          from rounds
+          join participants on rounds.id = participants.round_id
+          where rounds.status like 'waiting'
+          group by rounds.id
+          having count(participants.id) < 5;"),
+        join_error: "Too many players in that round, please choose another"
+      }
     else
-      participant = Participant.new(create_participant_params)
-      if participant.save
-        render json: { round: Round.find(participant.round_id) }
+      if Participant.where(round_id: create_participant_params[:round_id]).where(user_id: create_participant_params[:user_id])[0]
+        render json: { round: Round.find(create_participant_params[:round_id]) }
       else
-        render json: { error: participant.errors.full_messages }, status: :unprocessable_entity
+        participant = Participant.new(create_participant_params)
+        if participant.save
+          render json: { round: Round.find(participant.round_id) }
+        else
+          render json: { error: participant.errors.full_messages }, status: :unprocessable_entity
+        end
       end
     end
   end
--- app/javascript/react/containers/RoundsIndexContainer.js
+++ app/javascript/react/containers/RoundsIndexContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Redirect } from "react-router-dom"
+import { _ } from "lodash"
 
 import RoundTile from '../components/RoundTile'
 import NewRoundButton from '../components/NewRoundButton'
@@ -8,14 +9,11 @@ const RoundsIndexContainer = () => {
   const [rounds, setRounds] = useState([])
   const [user, setUser] = useState({
     id: null,
-    userName: null
+    user_name: null
   })
   const [round, setRound] = useState(null)
   const [busy, setBusy] = useState(false)
-
-  let setRoundID = (roundID) => {
-    setRound(roundID)
-  }
+  const [joinError, setJoinError] = useState(false)
 
   useEffect(() => {
     fetch("/api/v1/rounds")
@@ -33,7 +31,10 @@ const RoundsIndexContainer = () => {
     })
     .then((body) => {
       setRounds(body.rounds)
-      setUser(body.rounds[0].current_user)
+      setUser(body.current_user)
+      if (body.capped) {
+        setBusy(true)
+      }
     })
     .catch((error) => console.error(`Error in fetch: ${error.message}`))
   }, [])
@@ -59,10 +60,14 @@ const RoundsIndexContainer = () => {
     })
     .then(response => response.json())
     .then(body => {
+      debugger
       if (body.busy) {
-        setBusy(body.busy)
+        setBusy(true)
+      } else if (body.round) {
+        setRound(body.round.id)
       } else {
-        setRoundID(body.round.id)
+        setRounds(body.rounds)
+        setJoinError(true)
       }
     })
     .catch((error) => console.error(`Error in fetch: ${error.message}`))
@@ -76,7 +81,29 @@ const RoundsIndexContainer = () => {
     fetchPost(payload, endpoint)
   }
 
+  let busyMessage
+  let newRoundButtonColor
+  let joinRoundButtonColor
   let roundsTiles
+  let errorMessage
+
+  if (busy && user.id) {
+    newRoundButtonColor = "gray-button"
+    busyMessage = <p className="cell small-12 error-text">Too many rounds in progress, please try again in a few minutes</p>
+  } else if (user.id) {
+    newRoundButtonColor = "custom-button"
+    busyMessage = <></>
+  } else {
+    newRoundButtonColor = "gray-button"
+    busyMessage = <p className="cell small-12 error-text">Please log in to start or join a round</p>
+  }
+
+  if (user.id) {
+    joinRoundButtonColor = "custom-button"
+  } else {
+    joinRoundButtonColor = "gray-button"
+  }
+
   if (rounds.length < 1) {
     roundsTiles = "No active games"
   } else {
@@ -87,34 +114,31 @@ const RoundsIndexContainer = () => {
           round={round}
           user={user}
           joinRound={joinRound}
+          joinRoundButtonColor = {joinRoundButtonColor}
         />
       )
     })
+  }
+
+  if (joinError) {
+    errorMessage = <p className="callout alert cell small-12 error-text">Too many players in that round, please choose a different one</p>
+  } else {
+    <></>
   }
 
   if (round) {
     return <Redirect to={`/rounds/${round}`} />
   }
 
-  let newRoundButtonColor
-  let busyMessage
-  if (busy || rounds.length > 1) {
-    newRoundButtonColor = "gray-button"
-    busyMessage = <p className="cell small-12 error-text">Too many rounds in progress, please try again in a few minutes</p>
-  } else {
-    newRoundButtonColor = "custom-button"
-    busyMessage = <></>
-  }
-
   return (
     <div className="grid-container">
+      {errorMessage}
       <div className="grid-x center">
         <NewRoundButton
           user={user}
-          setRoundID={setRoundID}
+          setRound={setRound}
           createRound={createRound}
           roundCount={rounds.length}
-          busy={busy}
           newRoundButtonColor={newRoundButtonColor}
         />
         {busyMessage}
